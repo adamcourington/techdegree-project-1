2664b711564add051bc5af14584310060edd38d8
Adam Courington <adamcourington@gmail.com>
2020-05-15 09:22:14 -0500

    Added Quote Array

--- js/script.js
+++ js/script.js
@@ -10,6 +10,62 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: "I hear I forget, I see I remember, I do I understand.",
+    source: "Confucius"
+  },
+  {
+    quote: "Not all those who wander are lost.",
+    source: "J.R.R. Tolkien",
+    citation: "The Lord of the Rings",
+    year: "1954" 
+  },
+  {
+    quote: "Just keep swimming.",
+    source: "Dory",
+    citation: "Finding Nemo",
+    year: "2003" 
+  },
+  {
+    quote: "There's no crying in baseball!",
+    source: "Jimmy Dugan ",
+    citation: "A League of Their Own",
+    year: "1992" 
+  },
+  {
+    quote: "I feel the need - the need for speed!",
+    source: 'Lt. Pete "Maverick" Mitchell',
+    citation: "Top Gun",
+    year: "1986" 
+  },
+  {
+    quote: "Carpe diem.  Seize the day, boys. Make your lives extraordinary.",
+    source: "John Keating",
+    citation: "Dead Poets Society",
+    year: "1989" 
+  },
+  {
+    quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
+    source: "Mark Twain"
+  },
+  {
+    quote: "Those who dare to fail miserably can achieve greatly.",
+    source: "John F. Kennedy"
+  },
+  {
+    quote: "Love is a serious mental disease.",
+    source: "Plato"
+  },
+  {
+    quote: "It is our choices, that show what we truly are, far more than our abilities.",
+    source: "Albus Dumbledore",
+    citation: "Harry Potter and the Chamber of Secrets ",
+    year: "1998 " 
+  }
+
+];
+
 
 
 
